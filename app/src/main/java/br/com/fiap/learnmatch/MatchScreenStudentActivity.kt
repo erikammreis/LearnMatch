a88a6421af963a20c80c3475c0bec8ef7a2109dc
Guilherme Pendezza <guilherme.pendezza@pluto.tv>
2024-05-19 14:48:22 -0300

    init student screen

--- app/src/main/java/br/com/fiap/learnmatch/MatchScreenStudentActivity.kt
+++ app/src/main/java/br/com/fiap/learnmatch/MatchScreenStudentActivity.kt
@@ -2,14 +2,15 @@ package br.com.fiap.learnmatch
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MatchScreenStudentActivity : AppCompatActivity() {
     private lateinit var nameUser: TextView
     private lateinit var courseStudentMatch: TextView
-    private lateinit var buttonMatchMentor: Button
-    private lateinit var noMatchMentor: Button
+    private lateinit var buttonMatchMentor: ImageButton
+    private lateinit var noMatchMentor: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match_screen_student)
